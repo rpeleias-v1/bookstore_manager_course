c84e52b67fd0a2709af47b6b9f9d2b236d496935
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-25 23:28:14 -0300

    First commit: project structure created

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/BookstoremanagerApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.bookstoremanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookstoremanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookstoremanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigopeleias/bookstoremanager/BookstoremanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigopeleias.bookstoremanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoremanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
