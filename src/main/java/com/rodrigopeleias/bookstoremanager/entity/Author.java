3147ee71181585dbab3bfa583e6437a96065ea02
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-27 09:44:40 -0300

    Developed project entities

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Author.java
@@ -0,0 +1,32 @@
+package com.rodrigopeleias.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+
+
+}
---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/entity/Book.java
@@ -0,0 +1,47 @@
+package com.rodrigopeleias.bookstoremanager.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer pages;
+
+    @Column(nullable = false)
+    private Integer chapters;
+
+    @Column(nullable = false)
+    private String isbn;
+
+    @Column(name = "publisher_name", nullable = false, unique = true)
+    private String publisherName;
+
+    @ManyToOne(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REMOVE})
+    @JoinColumn(name = "author_id")
+    private Author author;
+}
