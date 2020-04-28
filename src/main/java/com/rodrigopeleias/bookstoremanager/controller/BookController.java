8fee4e8b9ff2adcb6555fb42d5584a548111c1b8
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-04-28 08:53:22 -0300

    Created post method

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
@@ -0,0 +1,30 @@
+package com.rodrigopeleias.bookstoremanager.controller;
+
+import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
+import com.rodrigopeleias.bookstoremanager.entity.Book;
+import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    private BookRepository bookRepository;
+
+    @Autowired
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @PostMapping
+    public MessageResponseDTO create(@RequestBody Book book) {
+        Book savedBook = bookRepository.save(book);
+        return MessageResponseDTO.builder()
+                .message("Book created with ID " + savedBook.getId())
+                .build();
+    }
+}
---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/dto/MessageResponseDTO.java
@@ -0,0 +1,11 @@
+package com.rodrigopeleias.bookstoremanager.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class MessageResponseDTO {
+
+    private String message;
+}
