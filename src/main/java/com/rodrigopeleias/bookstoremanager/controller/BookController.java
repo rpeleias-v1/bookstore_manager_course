18283423307a41896c4e5a34627c1bdf1a169ff9
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-05-01 18:37:37 -0300

    DEveloped get by id operation

--- src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
@@ -2,10 +2,10 @@ package com.rodrigopeleias.bookstoremanager.controller;
 
 import com.rodrigopeleias.bookstoremanager.dto.BookDTO;
 import com.rodrigopeleias.bookstoremanager.dto.MessageResponseDTO;
-import com.rodrigopeleias.bookstoremanager.entity.Book;
-import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
 import com.rodrigopeleias.bookstoremanager.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -27,5 +27,10 @@ public class BookController {
     @PostMapping
     public MessageResponseDTO create(@RequestBody @Valid BookDTO bookDTO) {
         return bookService.create(bookDTO);
+    }
+
+    @GetMapping("/{id}")
+    public BookDTO findById(@PathVariable Long id) {
+        return bookService.findById(id);
     }
 }
--- src/main/java/com/rodrigopeleias/bookstoremanager/service/BookService.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/service/BookService.java
@@ -8,6 +8,8 @@ import com.rodrigopeleias.bookstoremanager.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class BookService {
 
@@ -27,5 +29,10 @@ public class BookService {
         return MessageResponseDTO.builder()
                 .message("Book created with ID " + savedBook.getId())
                 .build();
+    }
+
+    public BookDTO findById(Long id) {
+        Optional<Book> optionalBook = bookRepository.findById(id);
+        return bookMapper.toDTO(optionalBook.get());
     }
 }
